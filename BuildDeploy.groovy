c5e485afb43c5988a4762e822013f542afcda0a9
pandu1031 <chintupandu1031@gmail.com>
2023-05-28 16:14:00 +0530

    added serverip parameter and added scp and ssh commands

--- BuildDeploy.groovy
+++ BuildDeploy.groovy
@@ -4,6 +4,7 @@ pipeline{
     parameters {
         string(name:'BRANCH_NAME', defaultValue: 'source', description: 'Enter source branch name')
         string(name:'BUILD_PIPE', defaultValue: 'pipeline', description: 'Enter pipeline branch name')
+        string(name:'SERVER_IP', defaultValue: '', description: 'Enter server ip')
     }
 
     stages{
@@ -24,10 +25,17 @@ pipeline{
         }
         stage("Upload artifacts"){
             steps{
-                println "here im uploading artifacts from jenkins server to tomcat"
+                println "here im uploading artifacts to s3 bucket"
                 sh "aws s3 ls"
                 sh "aws s3 ls s3://mamuu"
                 sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://mamuu/chintu/${BRANCH_NAME}/${BUILD_NUMBER}" 
+            }
+        }
+        stage("Deployment"){
+            steps{
+                println "deploying the artifacts to tomcat server"
+                sh "scp -i /tmp/mamu1031.pem/tmp/tomcatinstall.sh ec2-user@${SERVER_IP}:/tmp/"
+                sh "ssh -i /tmp/mamu1031.pem ec2-user@${SERVER_IP} \"bash /tmp/tomcatinstall.sh  && systemctl status tomcat\""
             }
         }
 
